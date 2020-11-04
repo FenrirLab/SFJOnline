9e59816916fdcd50564e3ec591835b0684457892
FenrirLab <43561935+FenrirLab@users.noreply.github.com>
2020-11-04 10:41:26 +0900

    Dev3.1Swiper導入
    自動再生とループ、バナー（？)を作ってもいいかもしれない

---
+++ js/swiper-initialize.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+      /*autoplay loop*/
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
