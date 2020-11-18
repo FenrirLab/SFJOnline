5e2f21443825b671ed59ddb8573f2f81c450f6c8
FenrirLab <43561935+FenrirLab@users.noreply.github.com>
2020-11-18 09:23:37 +0900

    Dev5.5
    math.jsの修正他微修正

--- js/math.js
+++ js/math.js
@@ -4,6 +4,8 @@ const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 const button4 = document.getElementById("button4");
 
+
+
 //ボタンをクリックしたときの処理
 button1.addEventListener("click",function(e){
 	e.preventDefault();
